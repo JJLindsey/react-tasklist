40b688cfd4c6bcfb7002ca1d09c492aac97d097f
JJLindsey <jlindsey010@gmail.com>
2021-08-27 11:23:38 -0700

    add/delete & set task reminders

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
         id: 2,
         text: 'Beach Day',
         day:'Sep 3rd at 9am',
-        reminder: false,
+        reminder: true,
     },
     {
         id: 3,
@@ -27,16 +28,33 @@ function App() {
     }
 ])
 
+//Add Task
+
+const addTask = (task) => {
+  const id =Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 //delete tasks
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+//toggle reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+    task.id === id ? { ...task, remider: !task.reminder} : task))
+}
+
   return (
     <div className='container'>
       <Header/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask} /> : 'No Tasks to Show'}
+      onDelete={deleteTask}
+      onToggle={toggleReminder} /> : 'No Tasks to Show'}
     </div>
   );
 }
