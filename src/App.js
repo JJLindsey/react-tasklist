2430a2a5b1b0dc52d22850d0312d65280418093e
JJLindsey <jlindsey010@gmail.com>
2021-08-27 11:34:54 -0700

    toggle button color w/state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import AddTask from './components/AddTask'
 
 function App() {
 
+  const [showAddTask, setShowAddTask] = useState(true )
+
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -50,8 +52,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-      <Header/>
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} 
       onDelete={deleteTask}
       onToggle={toggleReminder} /> : 'No Tasks to Show'}
